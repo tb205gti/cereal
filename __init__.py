ac22c2b8128929d9a2539ab912fd10e29d88015a
BogGyver <bogdan.butoi@gmail.com>
2020-04-16 01:45:42 -0400

    part of the tesla merge

--- __init__.py
+++ __init__.py
@@ -6,3 +6,6 @@ capnp.remove_import_hook()
 
 log = capnp.load(os.path.join(CEREAL_PATH, "log.capnp"))
 car = capnp.load(os.path.join(CEREAL_PATH, "car.capnp"))
+ui = capnp.load(os.path.join(CEREAL_PATH, "ui.capnp"))
+tinkla = capnp.load(os.path.join(CEREAL_PATH, "tinkla.capnp"))
+tesla = capnp.load(os.path.join(CEREAL_PATH, "tesla.capnp"))
